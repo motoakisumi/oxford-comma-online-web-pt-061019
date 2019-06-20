246b9445b132a71f6483c4a0f3de4791bdc3af19
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-20 16:20:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
     return array
+    
   elsif array.length == 2
     return array.join("and ")
+  
   else
     new_word = "and " + array.last
     array.pop
