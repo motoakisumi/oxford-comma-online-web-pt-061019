e0b3a09253f227da70c57602f2eef6577107ff61
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-20 16:22:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  
+  if array.length == 1
+    puts array
+    
+  elsif array.length == 2
+    puts array.join("and ")
+  
+  else
+    new_word = "and " + array.last
+    array.pop
+    array << new_word
+    puts array.join(", ")
 
 end
