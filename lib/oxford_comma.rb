cdac197c3b3324457ce947248c6548007eb1cd2a
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-20 16:18:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,6 @@ def oxford_comma(array)
   if array.length == 1
     array = array
   elsif array.length == 2
-    
     array.join("and ")
   elsif array.length > 2
     new_word = "and " + array.last
